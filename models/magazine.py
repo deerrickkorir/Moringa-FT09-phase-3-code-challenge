fa1fcc79d4f9f03750507a0f0a14d60850712b53
Derrick Korir <derrickroz4227@gmail.com>
2024-06-09 21:10:35 +0300

    final commit

--- models/magazine.py
+++ models/magazine.py
@@ -1,4 +1,4 @@
-# models/magazine.py
+
 
 class Magazine:
     def __init__(self, name=None, category=None):
@@ -15,6 +15,6 @@ class Magazine:
 
     @category.setter
     def category(self, value):
-        if not isinstance(value, str) or len(value.strip()) == 0:  # Check if value is a non-empty string
+        if not isinstance(value, str) or len(value.strip()) == 0:
             raise ValueError("Category must be a non-empty string")
         self._category = value
